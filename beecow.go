e8dc15f96323ce57334c47bca706082b7aa995fe
Dana NicCaluim <dniccaluim@airware.com>
2015-08-17 09:36:12 -0700

    Sleep from 10 to 6

--- beecow.go
+++ beecow.go
@@ -4,10 +4,12 @@ import (
 	"log"
 	"log/syslog"
 	"net/http"
+	"time"
 )
 
 type beeCow struct {
-	Logger *log.Logger
+	Logger   *log.Logger
+	Location *time.Location
 }
 
 func (bc beeCow) ServeHTTP(res http.ResponseWriter, req *http.Request) {
@@ -17,7 +19,12 @@ func (bc beeCow) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	} else if req.Method != "GET" {
 		res.WriteHeader(http.StatusMethodNotAllowed)
 	} else {
-		res.Write([]byte("<!doctype html><title>moo</title><pre style=margin:2em>(__)\n(<span id=m>oo</span>)\n \\/-------\\\n  ||     | \\\n  ||----||  *\n  ^^    ^^</pre><script>t=setTimeout;j=\"m.innerHTML='\";function b(x){z=Math.random();t(j+\"--';t(\\\"\"+j+\"oo';b(\"+(!x&z<.3)+\")\\\",100)\",(x?100:3e3)*++z)}b()</script>"))
+		hour := time.Now().In(bc.Location).Hour()
+		if hour < 22 && hour > 5 {
+			res.Write([]byte("<!doctype html><title>moo</title><pre style=margin:2em>(__)\n(<span id=m>oo</span>)\n \\/-------\\\n  ||     | \\\n  ||----||  *\n  ^^    ^^</pre><script>t=setTimeout;j=\"m.innerHTML='\";function b(x){z=Math.random();t(j+\"--';t(\\\"\"+j+\"oo';b(\"+(!x&z<.3)+\")\\\",100)\",(x?100:3e3)*++z)}b()</script>"))
+		} else {
+			res.Write([]byte("<!doctype html><title>moo</title><pre style=margin:2em>    <span id=z>ZzZ</span>\n(__)\n(--)-----\\\n \\/     ||\n  \\\\---//*\n   ^^ ^^</pre><script>j='z.innerHTML';setInterval(j+'='+j+'[1]+'+j+'[0]+'+j+'[1]',500)</script>"))
+		}
 	}
 }
 
@@ -26,7 +33,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	http.Handle("/", beeCow{logger})
+	location, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle("/", beeCow{logger, location})
 	err = http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
